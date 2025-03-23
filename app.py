fb4401a93136ec5f1d60a4185356ffc1e1dde571
Thanh-Phuog <phuongttt.125010122029@vtc.edu.vn>
2025-03-23 11:38:35 +0700

    commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from models.image_classifier import classify_image
 import requests
 from dotenv import load_dotenv
+import uuid
 
 app = Flask(__name__)
 load_dotenv()
@@ -16,23 +17,48 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 products = [
     {
+        "id": str(uuid.uuid4()),
         "name": "Giày búp bê lolita",
         "description": "Giày búp bê lolita nữ cao cấp quai ngọc cài độn đế phong cách tiểu thư",
         "image_url": "static/uploads/lolita.jpg",
         "predictions": "shoe_shop"
         
     },
+     {
+        "id": str(uuid.uuid4()),
+        "name": "Giày cao gót",
+        "description": "Giày cao gót nữ",
+        "image_url": "static/uploads/giaycaogot.jpg",
+        "predictions": "shoe_shop"
+        
+    },
     {
+        "id": str(uuid.uuid4()),
         "name": "Áo Dài",
         "description": "Áo  dài nữ ",
         "image_url": "static/uploads/ao_dai.jpg",
         "predictions": "áo dài"
     },
     {
+        "id": str(uuid.uuid4()),
         "name": "Điện thoại",
         "description": "Điện thoại cảm ứng",
         "image_url": "static/uploads/smartphone.jpg",
-        "predictions": "Điện thoại"
+        "predictions": "iPod"
+    },
+     {
+        "id": str(uuid.uuid4()),
+        "name": "Điện thoại Samsung Galaxy S21 Plus",
+        "description": "Điện thoại Samsung Galaxy S21 sở hữu thiết kế tuyệt đẹp.",
+        "image_url": "static/uploads/samsung.jpg",
+        "predictions": "iPod"
+    },
+     {
+        "id": str(uuid.uuid4()),
+        "name": "Điện thoại OPPO A17 ",
+        "description": "Thiết kế mỏng nhẹ, mặt lưng phủ giả da thời thượng",
+        "image_url": "static/uploads/oppo.jpg",
+        "predictions": "iPod"
     }
 ]
 
@@ -54,10 +80,11 @@ def upload_image():
     file.save(file_path)
     
     predictions = classify_image(file_path)
-    predictions_text = ", ".join(predictions)  # Chuyển list thành chuỗi
+    predictions_text = ", ".join(predictions) 
 
-       # Lưu sản phẩm vào danh sách
+
     product = {
+        "id": str(uuid.uuid4()),
         "name": request.form.get("product_name", "Không có tên"),
         "description": request.form.get("description", "Không có mô tả"),
         "image_url": f"static/uploads/{file.filename}",
@@ -65,7 +92,7 @@ def upload_image():
     }
     products.insert(0, product)
 
-    return render_template("index.html", predictions=predictions_text, image_url=url_for("static", filename=f"uploads/{file.filename}"), products=products, success=True)
+    return render_template("index.html", predictions=predictions_text, image_url=url_for("static", filename=f"uploads/{file.filename}"), products=products)
 
 # Phân tích cảm xúc
 @app.route("/analyze", methods=["POST"])
@@ -84,15 +111,18 @@ def analyze_sentiment():
     
     return jsonify(sentiment_results)
 
-@app.route("/product/<string:predictions>")
-def products_by_prediction(predictions):
-    related_products = [p for p in products if p.get("predictions") == predictions]
+@app.route("/product/<string:id>")
+def product_detail(id):
+    product = next((p for p in products if p["id"] == id), None)
+    
+    if not product:
+        return "Không tìm thấy sản phẩm", 404
+    
+    related_products = [p for p in products if p["predictions"] == product["predictions"] and p["id"] != id]
+
 
     if not related_products:
         return "Không có sản phẩm nào thuộc danh mục này", 404
-
-    # Lấy sản phẩm đầu tiên để hiển thị chi tiết
-    product = related_products[0]
 
     return render_template("product.html", product=product, related_products=related_products)
 
